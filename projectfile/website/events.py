8fae012818482d5fdd6cd610e3cfa0a38c907d15
Shreyans Amin <132426257+ShreyansAmin02@users.noreply.github.com>
2023-05-22 14:25:43 +1000

    Create events.py

---
+++ projectfile/website/events.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for
+from .models import Comment, Event
+from .forms import EventForm, CommentForm
+import os
+from werkzeug.utils import secure_filename
+from . import db
+from flask_login import login_required, current_user
+
+# handles different destiantion pages
+
+# a url prefix will refer to prefix for the url
+eventsbp = Blueprint('events', __name__, url_prefix='/events')
+
+
+# this will eventually be used to determine what destination is used
+@eventsbp.route("/<id>", methods=['GET', 'POST'])
+def show(id):  # not done yet
+    event = Event.query.filter_by(id=id).first()
+    cmtForm = CommentForm()
+    if id == None:
+        id = "events/create"
+    return render_template("events/details.html", event=event, form=cmtForm)
+
+
+@eventsbp.route("/create", methods=['GET', 'POST'])
+# @login_required (# uncomment once login function is complete)
+def create():
+    print('Method  type: ', request.method)
+    eventForm = EventForm()
+    if eventForm.validate_on_submit():
+        db_file_path = check_upload_file(eventForm)
+        event = Event(eventTitle=eventForm.title.data, style=eventForm.style.data, artistName=eventForm.artistName.data, address=eventForm.address.data, date_time=eventForm.dateTime.data, image=db_file_path,
+                      description=eventForm.description.data, tickets=eventForm.tickets.data,
+                      price=eventForm.price.data, contactDetails=eventForm.contactDetails.data)
+        db.session.add(event)
+        db.session.commit()
+        return redirect(url_for('events.create'))
+    return render_template('events/create.html', form=eventForm)
+
+
+def check_upload_file(form):
+    fp = form.image.data
+    filename = fp.filename
+    BASE_PATH = os.path.dirname(__file__)
+    upload_path = os.path.join(
+        BASE_PATH, 'static/image', secure_filename(filename))
+    db_upload_path = '/static/image/' + secure_filename(filename)
+    fp.save(upload_path)
+    return db_upload_path
+
+
+@eventsbp.route('<id>/comment', methods=['GET', 'POST'])
+# @login_required (# uncomment once login function is complete)
+def comment(id):
+    event_obj = Event.query.filter_by(id=id).first()
+    # here the form is created form = CommentForm()
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(
+            text=form.text.data,
+            event=event_obj, user=current_user)
+        db.session.add(comment)
+        db.session.commit()
+    # this refers to the name at the top... dummy
+    return redirect(url_for('event.show', id=id))
