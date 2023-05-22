1afc8d849118f490229675d56ad5a8246b819736
ShreyansAmin02 <shreyans.amin@connect.qut.edu.au>
2023-05-22 19:23:10 +1000

    Frishta Updates

--- projectfile/website/auth.py
+++ projectfile/website/auth.py
@@ -64,18 +64,4 @@ def logout():
     flash('User successfully logged out.')
     return redirect(url_for("auth.login"))
 
-
-#  im just tessting this partt
-#  im just tessting this partt
-#  im just tessting this partt
-#  im just tessting this partt
-#  im just tessting this partt
-<<<<<<< HEAD
-=======
-
-
-
 # yes testing this too
-
-
->>>>>>> 876162cfee8e7eaf9504bcb1f3be60cfd8aef91f
